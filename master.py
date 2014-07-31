fb187b580545dd07224a7450ef16fa83a87ba83b
John Safrit <jsafrit@users.noreply.github.com>
2014-07-31 16:12:09 -0400

    Added child modules for collection and data generation.

---
+++ master.py
@@ -0,0 +1,15 @@
+#------------------------------------------------------------------------------
+# Name:     master.py
+# Purpose:  Master module for talking to sensors and recording data 
+#           
+#
+# Author:   jsafrit
+# Created:  07/31/2014
+#------------------------------------------------------------------------------
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ sensor.py
@@ -0,0 +1,15 @@
+#------------------------------------------------------------------------------
+# Name:     sensor.py
+# Purpose:  Sensor module for simulating data collection 
+#           
+#
+# Author:   jsafrit
+# Created:  07/31/2014
+#------------------------------------------------------------------------------
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
