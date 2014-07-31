b3711c2d00880aa17675f718218df4c57919141f
John Safrit <jsafrit@users.noreply.github.com>
2014-07-31 16:09:07 -0400

    Added child process creation.

--- toplevel.py
+++ toplevel.py
@@ -11,6 +11,9 @@ import shlex,subprocess
 import time
 import sys
 
+MCOM = 'COM8'
+SCOM = 'COM9'
+
 def createVirtualComms(comm1='COM8', comm2='COM9'):
     '''create virtual com ports for testing link'''
     cmdline = 'socat PTY,link={} PTY,link={}'.format(comm1,comm2)
@@ -23,13 +26,32 @@ def createVirtualComms(comm1='COM8', comm2='COM9'):
         sys.exit(3)
     return pComms
 
+def createChildProcess(script,comm):
+    '''create child process connected to comm'''
+    cmdline = 'python3 {} {}'.format(script, comm)
+    args = shlex.split(cmdline)
+    pChild = subprocess.Popen(args)
+    #wait minimum time to ensure ports are up before returning
+    time.sleep(1)
+    if not pChild:
+        print('Attempt to create child failed. Exiting...')
+        sys.exit(4)
+    return pChild
+
+
 def main():
+    
+    #create virtual comms
     print('Calling {}...'.format('createVirtualComms'))
-    toplevel = createVirtualComms()
+    toplevel = createVirtualComms(MCOM,SCOM)
     if toplevel:
         print('Sucessful!')   
-        toplevel.terminate()
-        
+        #toplevel.terminate()
+    
+    masterChild = createChildProcess('master.py',MCOM)
+    slaveChild  = createChildProcess('sensor.py',SCOM)
+    
+    print('Complete.')
         
     
 
